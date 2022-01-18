05002efa58a63943de2fec3e61b5514eea723af6
MousamSingh <81910954+MousamS26@users.noreply.github.com>
2022-01-18 14:57:08 +0530

    Different T.C.

--- 96A.cpp
+++ 96A.cpp
@@ -8,8 +8,35 @@ int main()
 	freopen("out.txt", "w", stdout);
 #endif
 
+	//O(1) T.C.
 	char a[101];
 	gets(a);
 	puts(strstr(a, "0000000") || strstr(a, "1111111") ? "YES" : "NO");
 	return 0;
+
+	//O(n) T.C.
+	string s;
+	cin >> s;
+	int count(1);
+	bool checker = false;
+
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (s[i] == s[i + 1])
+			count++;
+		else
+			count = 1;
+		if (count >= 7)
+		{
+			checker = true;
+			break;
+		}
+	}
+	if (checker)
+		cout << "YES";
+	else
+		cout << "NO";
+	// cout << count << endl;
+
+
 }
