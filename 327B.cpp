75519ff8caa0e401801cbf2f320de078d59c95af
MousamSingh <81910954+MousamS26@users.noreply.github.com>
2022-01-25 12:56:13 +0530

    Add files via upload

---
+++ 327B.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+
+	int n, i(0);
+	for (cin >> n; i < n; cout << ++i + n << " ");
+	return 0;
+
+}
