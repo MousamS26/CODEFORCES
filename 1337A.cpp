12a8859314b8c01282da52a72d1d3e1472b0799f
MousamSingh <81910954+MousamS26@users.noreply.github.com>
2022-02-07 23:34:29 +0530

    Add files via upload

---
+++ 1337A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int t, a, b, c, d;
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+	cin >> t;
+	while (t--)
+	{
+		cin >> a >> b >> c >> d;
+		cout << b << " " << c << " " << c << endl;
+	}
+}
